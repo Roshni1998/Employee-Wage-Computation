aebf71bb1904dbacfb55bf13d910baa0740cd679
Roshni1998 <roshnimali847@gmail.com>
2021-06-02 01:16:10 +0530

    Added: EmployeeWage UC-9

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,7 +6,20 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 2;
 	public static final int IS_PART_TIME = 1;
 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHourPerMonth;
+	private int totalEmpWage;
+
+	public EmployeeWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.maxHourPerMonth = maxHourPerMonth;
+		this.numOfWorkingDays = numOfWorkingDays;
+	}
+
+	public void computeEmpWage() {
 		// Variables
 		int empHrs, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
@@ -26,15 +39,22 @@ public class EmployeeWageBuilder {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Employee Wage for " + company + " is : " + totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+	}
+
+	public String toString() {
+		return "Total Employee Wage for Company: " + company + " is : " + totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage("Amazon", 25, 4, 20);
-		computeEmpWage("Flipcart", 20, 2, 15);
-		computeEmpWage("Zomato", 15, 5, 35);
+		EmployeeWageBuilder amazon = new EmployeeWageBuilder("Amazon", 25, 4, 20);
+		EmployeeWageBuilder flipcart = new EmployeeWageBuilder("Flipcart", 20, 2, 15);
+		EmployeeWageBuilder zomato = new EmployeeWageBuilder("Zomato", 15, 5, 35);
+		amazon.computeEmpWage();
+		System.out.println(amazon);
+		flipcart.computeEmpWage();
+		System.out.println(flipcart);
+		zomato.computeEmpWage();
+		System.out.println(zomato);
 	}
-
 }
