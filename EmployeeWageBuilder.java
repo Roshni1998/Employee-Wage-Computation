e427e642ddeb2bf464238f2ebe3206542b7e53ad
Roshni1998 <roshnimali847@gmail.com>
2021-06-01 12:41:02 +0530

    Added: EmployeeWage-UC4

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,25 +2,28 @@ package com.EmployeeWage;
 
 public class EmployeeWageBuilder {
 
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Program");
-		// Constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
 		// Computation
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if (empcheck == IS_FULL_TIME) {
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		} else if (empcheck == IS_PART_TIME) {
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
-		} else {
+			break;
+		default:
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage: " + empWage);
+
 	}
 }
