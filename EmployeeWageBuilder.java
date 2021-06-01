6feb26a8e9a2f34f6a5e8aec0b25a4ef01114868
Roshni1998 <roshnimali847@gmail.com>
2021-06-01 12:29:06 +0530

    Added: EmployeeWage-UC2

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,5 +4,20 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
+		// Constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+		// Computation
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME) {
+			empHrs = 8;
+		} else {
+			empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
