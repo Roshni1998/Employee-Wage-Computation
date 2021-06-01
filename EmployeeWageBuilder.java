6fb6dd661e7f08c8beb66523f69debcab573e3eb
Roshni1998 <roshnimali847@gmail.com>
2021-06-01 12:49:00 +0530

    Added: EmployeeWage-UC6

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,12 +6,14 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 		// Variables
-		int empHrs, empWage = 0, totalEmpWage = 0;
+		int empHrs, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empcheck) {
 			case IS_FULL_TIME:
@@ -23,10 +25,11 @@ public class EmployeeWageBuilder {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: " + empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " + empHrs);
 		}
-		System.out.println("Total Employee Wage: " + totalEmpWage);
+		int TotalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Employee Wage: " + TotalEmpWage);
+
 	}
 }
